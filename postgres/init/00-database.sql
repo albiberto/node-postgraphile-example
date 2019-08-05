896fcf4621d0f0e7762ac902aeb9c12e5dfd6f96
Alberto Viezzi <aviezzi@outlook.com>
2019-08-06 00:38:22 +0200

    Init example

---
+++ postgres/init/00-database.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE "KingRoger";
+
+\connect "KingRoger";
+
+create table "Lights"
+(
+    "Id" uuid not null constraint "PK_Lights" primary key,
+    "LightType" text,
+    "Price" numeric not null,
+    "Enabled" boolean not null
+
+);
+
+alter table "Lights" owner to "RobotBoy";
